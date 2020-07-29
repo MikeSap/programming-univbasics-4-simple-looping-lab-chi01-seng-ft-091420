80dc3549f074344c478e33fe716ab5610a5e2697
Mike Sapienza <mikesapaudio@gmail.com>
2020-07-29 20:15:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,4 +23,11 @@ def loop_message_n_times (message, number)
     end
   end
   
+  def return_string_array (array)
+    i= 0
+    while i < array.length do
+      array[i]
+      i ++
+    end
+  end
   
